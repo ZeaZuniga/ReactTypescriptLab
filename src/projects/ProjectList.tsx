98ad9a6fb691b9f298dc2837b80acdcd6bcf7d26
Zealtiel <zealtiel@outlook.com>
2023-05-17 12:25:56 -0500

    Create projectList comp and pass down prop.

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,11 @@
+import { Project } from "./Project";
+
+interface ProjectListProps {
+    projects: Project[];
+}
+
+export default function ProjectList({projects}: ProjectListProps) {
+  return (
+    <pre>{JSON.stringify(projects, null, " ")}</pre>
+  )
+}
